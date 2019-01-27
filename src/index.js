e088cebd5966dbec61c072c087822e4140c1583f
Jeremy Greer <jex.grizzle@gmail.com>
2019-01-27 09:03:20 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+
+const renderApp = () =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./components/App', renderApp)
+}
+
+renderApp()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
