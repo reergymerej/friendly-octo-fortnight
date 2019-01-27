8544111fbf939cbfe5b8e917254d8e9bde35a1a3
Jeremy Greer <jex.grizzle@gmail.com>
2019-01-27 10:43:06 -0500

    Flip cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,31 @@
 import React from 'react'
 
-const Card = ({ front, back }) => (
-  <div className="flex">
-    <div className="flex-1 max-w-sm rounded overflow-hidden shadow-lg text-center m-8 p-8">
-      <div className="text-xs text-grey">
-        {front.tags.join(', ')}
+class Card extends React.Component {
+  state = {
+    showFront: true,
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({ showFront: !prevState.showFront }))
+  }
+
+  render() {
+    const { back, front } = this.props
+    const value = this.state.showFront ? front.value : back.value
+    return (
+      <div className="flex">
+        <div className="flex-1 cursor-pointer max-w-sm rounded overflow-hidden shadow-md hover:shadow-lg text-center m-8 p-8" onClick={this.handleClick}>
+          <div className="text-xs text-grey">
+            {front.tags.join(', ')} / {back.tags.join(', ')}
+          </div>
+          <div className="text-2xl my-2">
+            {value}
+          </div>
+        </div>
       </div>
-      <div className="text-2xl my-2">
-        {front.value}
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 const Cards = ({ cards }) => (
   <div>
